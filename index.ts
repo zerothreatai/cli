91c7b71976974cbd91983b475d033b2aecd52774
Ajay Ojha <ojhaajay@outlook.com>
2025-12-05 17:40:41 +0530

    fix: bad request on license activation

--- index.ts
+++ index.ts
@@ -1,22 +1,17 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import showMenu from './src/menu.js';
-import {startSetup} from './src/commands/start-setup.js';
-import { config } from 'dotenv'
+import { program } from "commander";
+import showMenu from "./src/menu.js";
+import { startSetup } from "./src/commands/start-setup.js";
+import { config } from "dotenv";
 
-config({
-  path: !process.env.WORKING_ENVIRONMENT ? `.env.prod`:`.env`
-});
+if (process.env.WORKING_ENVIRONMENT)
+  config({
+    path: `.env`,
+  });
 
-program
-  .name("zt")
-  .description("ZEROTHREAT AI CLI")
-  .version("1.0.0");
+program.name("zt").description("ZEROTHREAT AI CLI").version("1.0.0");
 
-program
-  .command("menu")
-  .description("Show main menu")
-  .action(showMenu);
+program.command("menu").description("Show main menu").action(showMenu);
 
 program.command("start-setup").action(startSetup);
 
--- src/config/api-config.ts
+++ src/config/api-config.ts
@@ -1,11 +1,18 @@
 export interface ApiEndpoints {
-    licenseApi: string;
-    onPremLicenseCloudeApi: string;
+  licenseApi: string;
+  onPremLicenseCloudeApi: string;
 }
 
-export const API_CONFIG = ():ApiEndpoints=>{return {
-    licenseApi: process.env.LICENSE_API as string,
-    onPremLicenseCloudeApi: process.env.CLOUD_API as string,
-}
-    
+export const API_CONFIG = (): ApiEndpoints => {
+  if (!process.env.WORKING_ENVIRONMENT) {
+    return {
+      licenseApi: `http://localhost:3201/api/license`,
+      onPremLicenseCloudeApi: `https://api.zerothreat.ai/api/on-prem/license`,
+    };
+  } else {
+    return {
+      licenseApi: process.env.LICENSE_API as string,
+      onPremLicenseCloudeApi: process.env.CLOUD_API as string,
+    };
+  }
 };
